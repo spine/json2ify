1a32832aef197a90e6fc33cb020475e5ca7f9f8b
Ben Nevile <ben@mainsocial.com>
2012-10-06 01:43:51 -0400

    "hygiene", as crockford called it.
    this change reflects current json2.js code.

--- index.js
+++ index.js
@@ -159,8 +159,9 @@
 // Create a JSON object only if one does not already exist. We create the
 // methods in a closure to avoid creating global variables.
 
-if (!this.JSON) {
-  this.JSON = {};
+var JSON;
+if (!JSON) {
+  JSON = {};
 }
 
 module.exports = JSON;
